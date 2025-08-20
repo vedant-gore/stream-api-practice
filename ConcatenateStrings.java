219c2af221f1a837cca60a68cb2d981f659dad37
Vedant Gore <vedantg.r10@gmail.com>
2025-08-20 09:30:03 +0530

    ConcatenateStrings

--- ConcatenateStrings.java
+++ ConcatenateStrings.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+// Question: Concatenate all strings in a list into a single string.
 public class ConcatenateStrings {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Stream", "API", "is", "powerful");
+        String concat = words.stream()
+                .collect(Collectors.joining(" "));
+        System.out.println("concat = " + concat);
+
+        String concat2 = words.stream()
+                .reduce((s1, s2) -> s1 + " " + s2)
+                .orElse("");
+        System.out.println("concat2 = " + concat2);
+
+        String concat3 = words.stream()
+                .reduce("", (s1, s2) -> s1 + " " + s2)
+                .trim();
+        System.out.println("concat3 = " + concat3);
+    }
 }
